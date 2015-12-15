cdca2e0ae2f98df93d8ad0f8b642104d9be481f5
Juan Jose Guevara <juanjose@acklenavenue.com>
2015-12-15 17:59:53 -0600

    rebuilding ellipsis after element becomes visible

--- src/angular-ellipsis.js
+++ src/angular-ellipsis.js
@@ -196,6 +196,13 @@ angular.module('dibari.angular-ellipsis', [])
 					buildEllipsis();
 				});
 
+				/**
+				*	Execute ellipsis truncate when element becomes visible
+				*/
+				scope.$watch(function() { return element.is(':visible') }, function() {
+    				asyncDigestDebounced.add(buildEllipsis);
+				});
+
 				function checkWindowForRebuild() {
 					if (attributes.lastWindowResizeWidth != window.innerWidth || attributes.lastWindowResizeHeight != window.innerHeight) {
 						buildEllipsis();
