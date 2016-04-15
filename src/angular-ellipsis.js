d94e897ed93e8fccb93037fc56ed3690357264ac
stephan <stephan@opaygo.com>
2016-04-15 16:15:26 +0200

    Implement ellipsis support for elements which are shown/hidden via ng-show directive

--- src/angular-ellipsis.js
+++ src/angular-ellipsis.js
@@ -54,6 +54,7 @@ angular.module('dibari.angular-ellipsis', [])
 	return {
 		restrict: 'A',
 		scope: {
+			ngShow: '=',
 			ngBind: '=',
 			ngBindHtml: '=',
 			ellipsisAppend: '@',
@@ -174,6 +175,13 @@ angular.module('dibari.angular-ellipsis', [])
 				/**
 				 *	Watchers
 				 */
+
+				/**
+				 *	Execute ellipsis truncate on ngBind update
+				 */
+				scope.$watch('ngShow', function() {
+					asyncDigestImmediate.add(buildEllipsis);
+				});
 
 				/**
 				 *	Execute ellipsis truncate on ngBind update
