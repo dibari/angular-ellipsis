d9fb9c38ebd5b31ce628cf34749ad2f91326666f
Adel Refaat <adelkamel@gmail.com>
2016-08-24 12:02:44 +1000

    Revert "Fixing an issue: Action assigned to data-ellipsis-append-click will be called more than one time"
    This reverts commit d016755290d27de5197c5f0bc0a3a225db23f221.

--- src/angular-ellipsis.js
+++ src/angular-ellipsis.js
@@ -17,148 +17,148 @@ angular.module('dibari.angular-ellipsis', [])
 
 .directive('ellipsis', ['$timeout', '$window', '$sce', function($timeout, $window, $sce) {
 
-    var AsyncDigest = function(delay) {
-        var timeout = null;
-        var queue = [];
+	var AsyncDigest = function(delay) {
+		var timeout = null;
+		var queue = [];
 
-        this.remove = function(fn) {
-            if (queue.indexOf(fn) !== -1) {
-                queue.splice(queue.indexOf(fn), 1);
-                if (queue.length === 0) {
-                    $timeout.cancel(timeout);
-                    timeout = null;
-                }
-            }
-        };
-        this.add = function(fn) {
-            if (queue.indexOf(fn) === -1) {
-                queue.push(fn);
-            }
-            if (!timeout) {
-                timeout = $timeout(function() {
-                    var copy = queue.slice();
-                    timeout = null;
-                    // reset scheduled array first in case one of the functions throws an error
-                    queue.length = 0;
-                    copy.forEach(function(fn) {
-                        fn();
-                    });
-                }, delay);
-            }
-        };
-    };
+		this.remove = function(fn) {
+			if (queue.indexOf(fn) !== -1) {
+				queue.splice(queue.indexOf(fn), 1);
+				if (queue.length === 0) {
+					$timeout.cancel(timeout);
+					timeout = null;
+				}
+			}
+		};
+		this.add = function(fn) {
+			if (queue.indexOf(fn) === -1) {
+				queue.push(fn);
+			}
+			if (!timeout) {
+				timeout = $timeout(function() {
+					var copy = queue.slice();
+					timeout = null;
+					// reset scheduled array first in case one of the functions throws an error
+					queue.length = 0;
+					copy.forEach(function(fn) {
+						fn();
+					});
+				}, delay);
+			}
+		};
+	};
 
-    var asyncDigestImmediate = new AsyncDigest(0);
-    var asyncDigestDebounced = new AsyncDigest(75);
+	var asyncDigestImmediate = new AsyncDigest(0);
+	var asyncDigestDebounced = new AsyncDigest(75);
 
-    return {
-        restrict: 'A',
-        scope: {
-            ngBind: '=',
-            ngBindHtml: '=',
-            ellipsisAppend: '@',
-            ellipsisAppendClick: '&',
-            ellipsisSymbol: '@',
-            ellipsisSeparator: '@',
-            useParent: "@",
-            ellipsisSeparatorReg: '='
-        },
-        compile: function(elem, attr, linker) {
+	return {
+		restrict: 'A',
+		scope: {
+			ngBind: '=',
+			ngBindHtml: '=',
+			ellipsisAppend: '@',
+			ellipsisAppendClick: '&',
+			ellipsisSymbol: '@',
+			ellipsisSeparator: '@',
+			useParent: "@",
+			ellipsisSeparatorReg: '='
+		},
+		compile: function(elem, attr, linker) {
 
-            return function(scope, element, attributes) {
-                /* Window Resize Variables */
-                attributes.lastWindowResizeTime = 0;
-                attributes.lastWindowResizeWidth = 0;
-                attributes.lastWindowResizeHeight = 0;
-                attributes.lastWindowTimeoutEvent = null;
-                /* State Variables */
-                attributes.isTruncated = false;
+			return function(scope, element, attributes) {
+				/* Window Resize Variables */
+				attributes.lastWindowResizeTime = 0;
+				attributes.lastWindowResizeWidth = 0;
+				attributes.lastWindowResizeHeight = 0;
+				attributes.lastWindowTimeoutEvent = null;
+				/* State Variables */
+				attributes.isTruncated = false;
 
-                function getParentHeight(element) {
-                    var heightOfChildren = 0;
-                    angular.forEach(element.parent().children(), function(child) {
-                        if (child != element[0]) {
-                            heightOfChildren += child.clientHeight;
-                        }
-                    });
-                    return element.parent()[0].clientHeight - heightOfChildren;
-                }
+				function getParentHeight(element) {
+					var heightOfChildren = 0;
+					angular.forEach(element.parent().children(), function(child) {
+						if (child != element[0]) {
+							heightOfChildren += child.clientHeight;
+						}
+					});
+					return element.parent()[0].clientHeight - heightOfChildren;
+				}
 
-                function buildEllipsis() {
-                    var binding = scope.ngBind || scope.ngBindHtml;
-                    var isTrustedHTML = false;
-                    if($sce.isEnabled() && angular.isObject(binding) && $sce.getTrustedHtml(binding)) {
-                        isTrustedHTML = true;
-                        binding = $sce.getTrustedHtml(binding);
-                    }
-                    if (binding) {
-                        var isHtml = (!(!!scope.ngBind) && !!(scope.ngBindHtml));
-                        var i = 0,
+				function buildEllipsis() {
+					var binding = scope.ngBind || scope.ngBindHtml;
+					var isTrustedHTML = false;
+					if($sce.isEnabled() && angular.isObject(binding) && $sce.getTrustedHtml(binding)) {
+						isTrustedHTML = true;
+						binding = $sce.getTrustedHtml(binding);
+					}
+					if (binding) {
+						var isHtml = (!(!!scope.ngBind) && !!(scope.ngBindHtml));
+						var i = 0,
 							ellipsisSymbol = (typeof(attributes.ellipsisSymbol) !== 'undefined') ? attributes.ellipsisSymbol : '&hellip;',
 							ellipsisSeparator = (typeof(scope.ellipsisSeparator) !== 'undefined') ? attributes.ellipsisSeparator : ' ',
 							ellipsisSeparatorReg = (typeof(scope.ellipsisSeparatorReg) !== 'undefined') ? scope.ellipsisSeparatorReg : false,
-							appendString = (typeof(scope.ellipsisAppend) !== 'undefined' && scope.ellipsisAppend !== '') ? ellipsisSymbol + "<span class='angular-ellipsis-append'>" + scope.ellipsisAppend + '</span>' : ellipsisSymbol,
+							appendString = (typeof(scope.ellipsisAppend) !== 'undefined' && scope.ellipsisAppend !== '') ? ellipsisSymbol + '<span>' + scope.ellipsisAppend + '</span>' : ellipsisSymbol,
 							bindArray = ellipsisSeparatorReg ? binding.match(ellipsisSeparatorReg) : binding.split(ellipsisSeparator);
 
-                        attributes.isTruncated = false;
-                        if (isHtml) {
-                            element.html(binding);
-                        } else {
-                            element.text(binding);
-                        }
+						attributes.isTruncated = false;
+						if (isHtml) {
+							element.html(binding);
+						} else {
+							element.text(binding);
+						}
 
-                        // If text has overflow
-                        if (isOverflowed(element, scope.useParent)) {
-                            var bindArrayStartingLength = bindArray.length,
+						// If text has overflow
+						if (isOverflowed(element, scope.useParent)) {
+							var bindArrayStartingLength = bindArray.length,
 								initialMaxHeight = scope.useParent ? getParentHeight(element) : element[0].clientHeight;
 
-                            if (isHtml) {
-                                element.html(binding + appendString);
-                            } else {
-                                element.text(binding).html(element.html() + appendString);
-                            }
-                            //Set data-overflow on element for targeting
-                            element.attr('data-overflowed', 'true');
+							if (isHtml) {
+								element.html(binding + appendString);
+							} else {
+								element.text(binding).html(element.html() + appendString);
+							}
+							//Set data-overflow on element for targeting
+							element.attr('data-overflowed', 'true');
 
-                            // Set complete text and remove one word at a time, until there is no overflow
-                            for (; i < bindArrayStartingLength; i++) {
-                                bindArray.pop();
+							// Set complete text and remove one word at a time, until there is no overflow
+							for (; i < bindArrayStartingLength; i++) {
+								bindArray.pop();
 
-                                if (isHtml) {
-                                    element.html(bindArray.join(ellipsisSeparator) + appendString);
-                                } else {
-                                    element.text(bindArray.join(ellipsisSeparator)).html(element.html() + appendString);
-                                }
+								if (isHtml) {
+									element.html(bindArray.join(ellipsisSeparator) + appendString);
+								} else {
+									element.text(bindArray.join(ellipsisSeparator)).html(element.html() + appendString);
+								}
 
-                                if ((scope.useParent ? element.parent()[0] : element[0]).scrollHeight < initialMaxHeight || isOverflowed(element, scope.useParent) === false) {
-                                    attributes.isTruncated = true;
-                                    break;
-                                }
-                            }
+								if ((scope.useParent ? element.parent()[0] : element[0]).scrollHeight < initialMaxHeight || isOverflowed(element, scope.useParent) === false) {
+									attributes.isTruncated = true;
+									break;
+								}
+							}
 
-                            // If append string was passed and append click function included
-                            if (ellipsisSymbol != appendString && typeof(scope.ellipsisAppendClick) !== 'undefined' && scope.ellipsisAppendClick !== '') {
-                                element.find('span.angular-ellipsis-append').bind("click", function(e) {
-                                    scope.$apply(function() {
-                                        scope.ellipsisAppendClick.call(scope, {
-                                            event: e
-                                        });
-                                    });
-                                });
-                            }
+							// If append string was passed and append click function included
+							if (ellipsisSymbol != appendString && typeof(scope.ellipsisAppendClick) !== 'undefined' && scope.ellipsisAppendClick !== '') {
+								element.find('span').bind("click", function(e) {
+									scope.$apply(function() {
+										scope.ellipsisAppendClick.call(scope, {
+											event: e
+										});
+									});
+								});
+							}
 
-                            if(!isTrustedHTML && $sce.isEnabled())
-                            {
-                                $sce.trustAsHtml(binding);
-                            }
-                        }
-                        else{
-                            element.attr('data-overflowed', 'false');
-                        }
-                    }
-                }
+							if(!isTrustedHTML && $sce.isEnabled())
+							{
+								$sce.trustAsHtml(binding);
+							}
+						}
+						else{
+							element.attr('data-overflowed', 'false');
+						}
+					}
+				}
 
-                /**
+				/**
 				 *	Test if element has overflow of text beyond height or max-height
 				 *
 				 *	@param element (DOM object)
@@ -166,81 +166,81 @@ angular.module('dibari.angular-ellipsis', [])
 				 *	@return bool
 				 *
 				 */
-                function isOverflowed(thisElement, useParent) {
-                    thisElement = useParent ? thisElement.parent() : thisElement;
-                    return thisElement[0].scrollHeight > thisElement[0].clientHeight;
-                }
+				function isOverflowed(thisElement, useParent) {
+					thisElement = useParent ? thisElement.parent() : thisElement;
+					return thisElement[0].scrollHeight > thisElement[0].clientHeight;
+				}
 
-                /**
+				/**
 				 *	Watchers
 				 */
 
-                /**
+				/**
 				 *	Execute ellipsis truncate on ngBind update
 				 */
-                scope.$watch('ngBind', function() {
-                    asyncDigestImmediate.add(buildEllipsis);
-                });
+				scope.$watch('ngBind', function() {
+					asyncDigestImmediate.add(buildEllipsis);
+				});
 
-                /**
+				/**
 				 *	Execute ellipsis truncate on ngBindHtml update
 				 */
-                scope.$watch('ngBindHtml', function() {
-                    asyncDigestImmediate.add(buildEllipsis);
-                });
+				scope.$watch('ngBindHtml', function() {
+					asyncDigestImmediate.add(buildEllipsis);
+				});
 
-                /**
+				/**
 				 *	Execute ellipsis truncate on ngBind update
 				 */
-                scope.$watch('ellipsisAppend', function() {
-                    buildEllipsis();
-                });
+				scope.$watch('ellipsisAppend', function() {
+					buildEllipsis();
+				});
 
-                /**
+				/**
 				*	Execute ellipsis truncate when element becomes visible
 				*/
-                scope.$watch(function() { return element[0].offsetWidth != 0 && element[0].offsetHeight != 0 }, function() {
-                    asyncDigestDebounced.add(buildEllipsis);
-                });
+				scope.$watch(function() { return element[0].offsetWidth != 0 && element[0].offsetHeight != 0 }, function() {
+					asyncDigestDebounced.add(buildEllipsis);
+				});
 
-                function checkWindowForRebuild() {
-                    if (attributes.lastWindowResizeWidth != window.innerWidth || attributes.lastWindowResizeHeight != window.innerHeight) {
-                        buildEllipsis();
-                    }
+				function checkWindowForRebuild() {
+					if (attributes.lastWindowResizeWidth != window.innerWidth || attributes.lastWindowResizeHeight != window.innerHeight) {
+						buildEllipsis();
+					}
 
-                    attributes.lastWindowResizeWidth = window.innerWidth;
-                    attributes.lastWindowResizeHeight = window.innerHeight;
-                }
+					attributes.lastWindowResizeWidth = window.innerWidth;
+					attributes.lastWindowResizeHeight = window.innerHeight;
+				}
 
-                var unbindRefreshEllipsis = scope.$on('dibari:refresh-ellipsis', function() {
-                    asyncDigestImmediate.add(buildEllipsis);
-                });
-                /**
+				var unbindRefreshEllipsis = scope.$on('dibari:refresh-ellipsis', function() {
+					asyncDigestImmediate.add(buildEllipsis);
+				});
+				/**
 				 *	When window width or height changes - re-init truncation
 				 */
 
-                function onResize() {
-                    asyncDigestDebounced.add(checkWindowForRebuild);
-                }
+				function onResize() {
+					asyncDigestDebounced.add(checkWindowForRebuild);
+				}
 
-                var $win = angular.element($window);
-                $win.bind('resize', onResize);
+				var $win = angular.element($window);
+				$win.bind('resize', onResize);
 
-                /**
+				/**
 				 * Clean up after ourselves
 				 */
-                scope.$on('$destroy', function() {
-                    $win.unbind('resize', onResize);
-                    asyncDigestImmediate.remove(buildEllipsis);
-                    asyncDigestDebounced.remove(checkWindowForRebuild);
-                    if (unbindRefreshEllipsis) {
-                        unbindRefreshEllipsis();
-                        unbindRefreshEllipsis = null;
-                    }
-                });
+				scope.$on('$destroy', function() {
+					$win.unbind('resize', onResize);
+					asyncDigestImmediate.remove(buildEllipsis);
+					asyncDigestDebounced.remove(checkWindowForRebuild);
+					if (unbindRefreshEllipsis) {
+						unbindRefreshEllipsis();
+						unbindRefreshEllipsis = null;
+					}
+				});
 
 
-            };
-        }
-    };
+			};
+		}
+	};
 }]);
