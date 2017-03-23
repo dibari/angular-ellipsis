a3424d319395177187d881d9b38573054282e4f4
Alon Bar David <me@alonbd.com>
2017-03-23 09:52:06 +0200

    remove implicit dependency on jQuery
    built in angular.find only works support element name for selectors. As such there is an implicit dependency on jQuery by using element.find.

--- src/angular-ellipsis.js
+++ src/angular-ellipsis.js
@@ -153,7 +153,8 @@ angular.module('dibari.angular-ellipsis', [])
 
 							// If append string was passed and append click function included
 							if (ellipsisSymbol != appendString && typeof(scope.ellipsisAppendClick) !== 'undefined' && scope.ellipsisAppendClick !== '') {
-								element.find('span.angular-ellipsis-append').bind("click", function(e) {
+								var clickArea = angular.element(element[0].querySelector('span.angular-ellipsis-append'));
+                                				clickArea.bind("click", function(e) {
 									scope.$apply(function() {
 										scope.ellipsisAppendClick.call(scope, {
 											event: e
